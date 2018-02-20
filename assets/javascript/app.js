07c474decd9fa24c54eb6313df79c6ecc88bb5ad
mgamatero <mgamatero@yahoo.com>
2018-02-19 23:54:51 -0800

    gif now https links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,105 +6,113 @@ $(document).ready(function () {
         question: "What is the largest bone in the human body?",
         answers: ["Femur", "Lemur", "Radius", "Ulna"],
         correct: 0,
-        image: "assets/images/femur.gif",
+        image: "https://media.giphy.com/media/IvhprlIKgLbaw/giphy.gif",
     },
 
     {
         question: "What is the largest two digit prime number?",
         answers: ["97", "67", "37", "91"],
         correct: 0,
-        image: "assets/images/prime.gif"
+        image: "https://media.giphy.com/media/nSfyYGO3LMGjK/giphy.gif"
     },
 
     {
         question: "Which pop album is the best selling of all time?",
         answers: ["Sgt. Pepper's Lonely Hearts Club Band", "Thriller", "Jagged Little Pill", "The Chronic"],
         correct: 1,
-        image: "assets/images/thriller.gif"
+        image: "https://media.giphy.com/media/ZXD5JEmd2VFxC/giphy.gif"
     },
     {
         question: "Which of the following can NOT be said of Will in “Good Will Hunting?",
         answers: ["He has been in trouble with the law", "He has trouble holding jobs", "He comes from a good family", "He is a Genius"],
         correct: 2,
-        image: "assets/images/goodwillhunting.gif"
+        image: "https://media.giphy.com/media/Md2HLNKZL2YEw/giphy.gif"
     },
     {
         question: "In “Inception,” the spinning top is an example of a/an:",
         answers: ["Idol", "Hex", "Good Luck Charm", "Totem"],
         correct: 3,
-        image: "assets/images/totem.gif"
+        image: "https://media.giphy.com/media/Gw6zuulR3WKlO/giphy.gif"
     },
     {
         question: "What happens when Aaron gets a shot at anchoring the nightly news on “Broadcast News?",
         answers: ["He is unable to speak", "The teleprompter breaks", "There is a power outage at the studio", "He has a sweating attack"],
         correct: 3,
-        image: "assets/images/sweat.gif"
+        image: "https://media.giphy.com/media/4bWWKmUnn5E4/giphy.gif"
     },
     {
         question: "What is the most common element on Earth?",
         answers: ["Hydrogen", "Gold", "Sodium", "Silver"],
         correct: 0,
-        image: "assets/images/hydrogen.gif"
+        image: "https://media.giphy.com/media/oMOO5py01rwGs/giphy.gif"
     },
     {
         question: "Which Ocean goes to the deepest depths?",
         answers: ["Pacific", "Atlantic", "Indian", "Arctic"],
         correct: 0,
-        image: "assets/images/pacific.gif"
+        image: "https://media.giphy.com/media/11i4hptC71c90c/giphy.gif"
     },
     {
         question: "How many sides does a heptagon have?",
         answers: ["5", "3", "7", "9"],
         correct: 2,
-        image: "assets/images/heptagon.gif"
+        image: "https://media.giphy.com/media/eFmm3rq114lR6/giphy.gif"
     },
     {
         question: "What does HTML stand for?",
-        answers: ["Hot Tamales Meal Large", "Hey There My Lady", "Typer Hex Markup Language", "Hyper Text Markup Language"],
+        answers: ["Hot Tamales Meal Large", "Hey There My Lady", "Typer Hext Markup Language", "Hyper Text Markup Language"],
         correct: 3,
-        image: "assets/images/html.gif"
+        image:"https://media.giphy.com/media/sl444NqppDOBa/giphy.gif"
+        // image: "assets/images/html.gif"
     },
     {
         question: "Which is the most populous state?",
         answers: ["New York", "Utah", "Oregon", "California"],
         correct: 3,
-        image: "assets/images/california.gif"
+        image:"https://media.giphy.com/media/2Wf4E5HlJhM0IrVV0x/giphy.gif"
+        // image: "assets/images/california.gif"
     },
     {
         question: "Who is trying to seduce Benjamin in The Graduate?",
         answers: ["Mr. Robinson", "Ms. Robinson", "Mrs. Robinson", "Robinson Crusoe"],
         correct: 2,
-        image: "assets/images/mrsrobinson.gif"
+        image: "https://media.giphy.com/media/6O9fkxYQ1dbG/giphy.gif"
     },
     {
         question: "Who interrupted Taylor Swift's acceptance speech at the 2009 Video Music Awards?",
         answers: ["Snoop", "Jay-Z", "Kanye", "Britney Spears"],
         correct: 2,
-        image: "assets/images/kanye.gif"
+        image: "https://media.giphy.com/media/1hjmySJVirhOU/giphy.gif"
     },
     {
         question: "How many items are in a bakers dozen?",
         answers: ["13", "12", "11", "14"],
         correct: 0,
-        image: "assets/images/bakersdozen.gif"
+        image: "https://media.giphy.com/media/6iuLysv5iB3bO/giphy.gif"
     },
     {
         question: "Entomology is the branch of science that studies what?",
         answers: ["Plants", "Music", "Animals", "Insects"],
         correct: 3,
-        image: "assets/images/entomology.gif"
+        image: "https://media.giphy.com/media/Yuvl9zDgVNCcU/giphy.gif"
     },
     {
         question: "What is the traditional fermented Korean side dish made with seasoned vegetables and salt?",
         answers: ["Kimchi", "Bulgogi", "Galbi", "Bibimbap"],
         correct: 0,
-        image: "assets/images/kimchi.gif"
+        image: "https://media.giphy.com/media/xUPJPhnAzy86MR7WmI/giphy.gif"
+    },
+    {
+        question: "Who played Darth Vader on the set of the movie?",
+        answers: ["David Prowse", "Peter Mayhew", "Anthony Daniels", "James Earl Jones"],
+        correct: 0,
+        image: "https://media.giphy.com/media/RvV4sUKIvrzGM/giphy.gif"
     },
     {
         question: "What is the square root of 256?",
         answers: ["36", "16", "14", "17"],
         correct: 1,
-        image: "assets/images/squareroot.gif"
+        image: "https://media.giphy.com/media/DHqth0hVQoIzS/giphy.gif"
     }
     ]
 
@@ -163,12 +171,15 @@ $(document).ready(function () {
 
     // function that displays answer and transitions once answer is picked
     function answerAnimation(currQuest, correct, img) {
+        $("#transition").hide()
         $("#questions").hide()
-        $("#transition").show()
 
         $("#transitionQuestion").html(currQuest)
         $("#correctAnswer").html(correct)
         $('#transitionImg').attr("src", img)
+
+        $("#transition").show()
+   
 
         var answerAnimationDelay = setTimeout(function () {
             $("#transition").hide()
